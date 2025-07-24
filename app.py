133645f6129ffcf032ea2444c253f47939ed98c8
diortiz <82378543+diortiz@users.noreply.github.com>
2025-07-24 16:52:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+
+
+st.set_page_config(page_title="Focus List - Fund Selection", layout ="wide")
 st.title("Focus List - Fund Selection")
 st.write("Estas viendo el embrión de una nueva la nueva Focus List.")
 
