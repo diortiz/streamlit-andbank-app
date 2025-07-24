5736f110ea98398c55d0d54e0482114a86811479
diortiz <82378543+diortiz@users.noreply.github.com>
2025-07-24 16:37:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Focus List - Fund Selection")
 st.write("Estas viendo el embrión de una nueva la nueva Focus List.")
 
 def cargar_datos():
-  return pd.read_excel("prueba.xlsx")
+  return pd.read_csv("prueba.csv")
 
 df = cargar_datos()
 
