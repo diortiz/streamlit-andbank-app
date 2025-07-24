03a0660354d4c733496bdc1a6609006b11a8a96e
diortiz <82378543+diortiz@users.noreply.github.com>
2025-07-24 14:06:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,13 @@ import streamlit as st
 
 st.title("🌐 Web Vacía - Tema Andbank")
 st.write("Esta es una plantilla mínima con el tema corporativo aplicado.")
+
+opciones = ["Elige una categoría", "Renta Variable","Renta Fija"]
+
+seleccion = st.selectbox("Elige una opción:",opciones)
+
+if seleccion != "Elige una categoría":
+  st.success(f"Has seleccionado: {seleccion}")
+
+else:
+  st.info("Por favor, selecciona una opción del menú.")
